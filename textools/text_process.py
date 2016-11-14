7cb27e1d08232d18c53209833a12eabc6d9782c8
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-11-14 17:27:52 +0100

    Fix: text_process.py non-ascii characters cleaning

--- textools/text_process.py
+++ textools/text_process.py
@@ -10,6 +10,6 @@ def clean(text):
     text = re.sub(r'(?:http[s]?://)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
     # Remove non ascii characters
     regex = re.compile(ur'[^\x00-\x7F]+', re.UNICODE)
-    row = re.sub(regex, '', row)
+    text = re.sub(regex, '', text)
 
     return text
